169eb1e01188fc20729c91ce8e8b3186b3de310e
BirkBagge <birk.33bagge@gmail.com>
2023-05-14 18:50:04 +0300

    Movement ja map

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public float Turn = 1;
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+
+    }
+
+
+    private void Update()
+    {   // asettaa nopeuden eteenpäin
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+
+        // vasemmalle ja oikealle kun painetaan a ja d
+        if (Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * Turn * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * Turn * Time.deltaTime;
+        }
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {   //törmäyksessä vertaa tag ja laittaa päälle "restart"
+        if (collision.gameObject.CompareTag("Auto"))
+        {
+            Invoke("Restart", 1f);
+
+        }
+
+    }
+    void Restart()
+    {   //lataa scenen uudelleen
+        SceneManager.LoadScene("SampleScene");
+    }
+}
